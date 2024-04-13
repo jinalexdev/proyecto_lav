f00b4b7ff3ea99dccc1610025e37d2b2b3bfde1a
jinalex <alexmega929@gmail.com>
2024-04-13 17:13:08 +0200

    Primer comit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+     
+    return view('welcome');
+});
+
+Route::get('/prueba', function () {
+     
+    return "hola esto es una prueba";
+});
